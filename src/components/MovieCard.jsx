92e57ecf87d44a827e035894042c1168d5dce013
arseniypom <arpomazkov@bk.ru>
2025-12-24 07:26:08 +0000

    deleted Button code & localstorage

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,29 +1,9 @@
-import { useEffect, useState } from "react";
-
 export default function MovieCard({ movie }) {
-  const [isFav, setIsFav] = useState(false);
-
-  useEffect(() => {э
-    const favs = JSON.parse(localStorage.getItem("favorites")) || [];-х
-    setIsFav(favs.some((m) => m.id === movie.id));
-  }, [movie.id]);
-
-  const toggleFavorite = () => {
-    const favs = JSON.parse(localStorage.getItem("favorites")) || [];
-
-    const updated = isFav
-      ? favs.filter((m) => m.id !== movie.id)
-      : [...favs, movie];
-
-    localStorage.setItem("favorites", JSON.stringify(updated));
-    setIsFav(!isFav);
-  };
-
   return (
     <div className="relative bg-white rounded-xl shadow hover:shadow-lg transition max-w-[220px] w-full mx-auto">
-      <button onClick={toggleFavorite} className="absolute top-3 right-3 z-10 text-2xl">
-      {isFav ? "❤️" : "🤍"}
-      </button>
+      {/* <button className="absolute top-3 right-3 z-10 text-2xl">
+      {"❤️"}
+      </button> */}
 
       <div className="relative aspect-[2/3] overflow-hidden rounded-t-xl">
         <img
--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -1,25 +1,13 @@
-import { useEffect, useState } from "react";
-import MovieList from "../components/MovieList";
+import MovieList from '../components/MovieList';
 
 export default function FavoritesPage() {
-  const [favorites, setFavorites] = useState([]);
-
-  useEffect(() => {
-    const data = localStorage.getItem("favorites");
-    setFavorites(data ? JSON.parse(data) : []);
-  }, []);
-
-  if (!favorites.length) {
-    return (
-      <p className="text-center mt-10 text-gray-500">
-        Нет избранных фильмов
-      </p>
-    );
-  }
-
   return (
-    <div className="pt-10">
-      <MovieList movies={favorites} />
-    </div>
+    <p className="text-center mt-10 text-gray-500">Нет избранных фильмов</p>
   );
+
+  // return (
+  //   <div className="pt-10">
+  //     <MovieList movies={favorites} />
+  //   </div>
+  // );
 }
